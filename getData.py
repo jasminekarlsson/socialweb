881cb661a19615464ceb8ef8362015b320aa2f62
mariahotoiu <mariahotoiu@yahoo.com>
2018-03-08 10:52:29 +0100

    new functions for retrieving data added

--- getData.py
+++ getData.py
@@ -19,9 +19,38 @@ def getUsers():
     results = cursor.fetchall()
     db.close()
     return results
+
+
+def getCategories():
+    db = MySQLdb.connect("localhost","mariahotoiu","copacroz","yelp_db" )
+    cursor = db.cursor()
+    query ="SELECT business_id, category FROM Category"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    columns = ['business_id','category']
+    categories = []
+    for row in results:
+        categories.append(dict(zip(columns,row)))
+    return categories
+    db.close()
+    
+
+
+def getAttributes():
+    db = MySQLdb.connect("localhost","mariahotoiu","pass","yelp_db" )
+    cursor = db.cursor()
+    query ="SELECT business_id, name, value FROM Attribute LIMIT 10"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    columns = ['business_id','name', 'value']
+    attributes = []
+    for row in results:
+        attributes.append(dict(zip(columns,row)))
+    return attributes
+    db.close() 
    
 def getReviews():
-    db = MySQLdb.connect("localhost","mariahotoiu","copacroz","yelp_db" )
+    db = MySQLdb.connect("localhost","mariahotoiu","pass","yelp_db" )
     columns = ['id','business_id','user_id','stars','date','text']
     cursor = db.cursor()
     query ="SELECT id,business_id,user_id,stars,date,text FROM Review Limit 10"
@@ -29,7 +58,6 @@ def getReviews():
     results = cursor.fetchall()
     db.close()
     reviews = []
-    print results[1]
     for row in results:
         reviews.append(dict(zip(columns,row)))
     random.shuffle(reviews)
@@ -40,6 +68,29 @@ def getReviews():
     d = Data(testData,trainData)    
     return d
 
+
+def isFriend(user_id1, user_id2):
+    db = MySQLdb.connect("localhost","mariahotoiu","pass","yelp_db" )
+    cursor = db.cursor()
+    query = """SELECT COUNT(*) FROM Friend WHERE user_id = '%s'\
+                AND friend_id = '%s'""" %(user_id1, user_id2)
+    cursor.execute(query)
+    results = cursor.fetchall()
+    db.close()
+    return bool(results)
+
+
+
+
+
+#fr = isFriend ('oMy_rEb0UBEmMlu-zcxnoQ', 'cvVMmlU1ouS3I5fhutaryQ')
+#print fr
+
+#categories = getCategories()
+#print categories[1]
+
+#attributes = getAttributes()
+#print attributes[3]
 
 #d = getReviews()
 #testData = d.testData
