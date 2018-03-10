770df4cbcaff2430df32f46c1bd1235ba2c0e70e
Jasmine Karlsson <j.a.c.karlsson@student.vu.nl>
2018-03-10 18:53:57 +0100

    Add function similarity

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import getData
-import evaluation
-import matrix
+# import evaluation
+# import matrix
+import similar
 
-reviews = getData.getReviews()
-train_reviews = reviews['trainData']
-test_reviews = reviews['testData']
+# reviews = getData.getReviews()
+# train_reviews = reviews['trainData']
+# test_reviews = reviews['testData']
+
+simArrays = similar.getSimilarity()
+user1 = simArrays[0]
+user2 = simArrays[1]
+similarity = simArrays[2]
+# for i in range(0, len(user1)):
+#     print "Users: " + user1[i] + " and " + user2[i] + " has similarity " + str(similarity[i])
+
 
 # TODO compute similarities matrix
 matrix = None
 
-evaluation.evaluate(matrix, train_reviews, test_reviews)
+# evaluation.evaluate(matrix, train_reviews, test_reviews)
--- similar.py
+++ similar.py
@@ -1,22 +1,51 @@
 import MySQLdb
+import collections as cl
 
-db = MySQLdb.connect("localhost","root","pass","yelp_db")
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
+#Calculates simularity between given user1 and user2
+def calculatSimularity(user1, user2):
+    sim = 5 - abs(user1[8] - user2[8])
+    # print "Users: " + user1[1] + " and " + user2[1] + " has similarity " + str(sim)
+    return sim
 
-sql = "SELECT * FROM user"
 
-try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Fetch all the rows in a list of lists.
-   results = cursor.fetchall()
-   for row in results:
-      name = row[0]
-      # Now print fetched result
-      print "name=" + name
-except:
-   print "Error: unable to fecth data"
+def getSimilarity():
+    db = MySQLdb.connect("localhost","root","pass","yelp_db")
+    db2 = MySQLdb.connect("localhost","root","pass","yelp_db")
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    cursor2 = db2.cursor()
+    sql = "SELECT * FROM User LIMIT 5000"
 
-# disconnect from server
-db.close()
+    # Execute the SQL command
+    cursor.execute(sql)
+    # Fetch all the rows in a list of lists.
+    results = cursor.fetchall()
+    number = 0
+    user1 = []
+    user2 = []
+    similarity = []
+    rows = 0
+    column = 0
+    #Goes through all the users in a nested loop
+    for row in results:
+        cursor2.execute(sql)
+        for rowx in results:
+            #Check if user1 and user2 is the same
+            if row[0]  == rowx[0]:
+                print row[1]
+            else:
+                # number = number +
+                #Append user1, user2 and similarity to 3 different arrays
+                user1.append(row[0])
+                user2.append(row[0])
+                similarity.append(calculatSimularity(row, rowx))
+
+    # Now print fetched result
+    # print number
+    Data = cl.namedtuple('Data',['user1','user2',"similarity"])
+    d = Data(user1,user2,similarity)
+    # disconnect from server
+    db.close()
+    return d
+
+#getSimilarity()
